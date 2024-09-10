c3f21aec9da3e77911d566e49e475c3373e8c46b
neha601 <nehuejaz@gmail.com>
2024-09-10 04:35:29 -0700

    update

---
+++ src/app/address/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Image from "next/image";
+const page = () => {
+    return (
+   <div>Gulberg Garden RYK</div>
+    )
+}
+
+export default page 
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Image from "next/image";
+const page = () => {
+    return (
+   <div>03033269257</div>
+    )
+}
+
+export default page 
